4db68aefe0ff29a6cbcae9d7769e4806561d5f52
ziyad <ziadzaher2525@gmail.com>
2023-08-09 12:40:39 -0400

    enddddddddddddddd

---
+++ Day3/move.js
@@ -0,0 +1,20 @@
+let numb = 42
+const Tostr = String(numb)
+let str = "25"
+const tonum = Number(str)
+// التحديي الث
+let text = "10"
+text = Number(text)
+console.log(text + 9)
+// التخدي الثالث
+let $num = "15.5"
+$num = parseFloat($num)
+console.log($num / 2);
+//---------------------------------
+let str1 = "5"
+let num2 = 3
+console.log(str1 + num2); 
+// ----------------------------------------
+let boolean = true
+let num1 = 1
+console.log(boolean+num1);git 
