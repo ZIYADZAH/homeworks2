09f81ad93d9a95f092cbfa0b36e23bcdfa4d4c11
ziyad <ziadzaher2525@gmail.com>
2023-08-11 14:00:02 -0400

    first week of js

---
+++ day6/move.js
@@ -0,0 +1,125 @@
+// // fitrst week hereeeeeee
+
+// //let va
+// let username = "Ziyad"
+// //X2
+// let age=17;
+
+// //const 
+// const birthday="2000/1/1"
+
+// //opjecttss
+// let opject_={
+
+//     username : "Ziyad",
+//     age : 17,
+// }
+// // الزائد  الناقص
+// let text_ = "text"
+
+// const pp=22+"1"
+
+// const p=22-"1"
+
+// console.log(`${pp}:${p}`);  
+
+
+// let str = "test"
+// console.log(str);
+// // scope
+// {
+//     let user_1="test"
+//     const uuuuu=19;
+// }
+// // double
+// let numdo =1.1
+
+// console.log(typeof numdo);
+// // intger 
+// let numin = 10
+
+// console.log(typeof numin);
+// // null
+// let null1=null;
+
+// console.log(typeof null1);
+// // boolen
+// let tt=(1 > 3)
+
+// console.log(tt);
+// // string
+// let string = "hello word "
+// console.log(string);
+
+
+// // str to number
+// const str_1="11";
+// const NumTostr=Number(str_1)
+// console.log(`${typeof str_1}: ${str_1}`);
+// console.log(`${typeof NumTostr}: ${NumTostr}`);
+
+// // str to number
+// const str1="text";
+// const NumToStr=Number(str1)
+// console.log(`${typeof str1}: ${str1}`);
+// console.log(`${typeof NumToStr}: ${NumToStr}`);
+
+// // number to number 
+// const num1=5;
+// const NumToNum=Number(num1)
+// console.log(`${typeof num1}: ${num1}`);
+// console.log(`${typeof NumToNum}: ${NumToNum}`);
+
+
+// // Boolean to number
+// const bool_2=true;
+// const BoolTonum=Number(bool_2);
+// console.log(`${typeof bool_2}: ${bool_2}`);
+// console.log(`${typeof BoolTonum}: ${BoolTonum}`);
+
+// // undefined to number 
+
+// const Un_1=undefined;
+// const UnTonum=String(Un_1)
+// console.log(`${typeof Un_1}: ${Un_1}`);
+// console.log(`${typeof UnTonum}: ${UnTonum}`);
+
+
+// // Null To number 
+
+// const null_2=null;
+// const nullTonum=Number(null_1)
+// console.log(`${typeof null_2}: ${null_2}`);
+// console.log(`${typeof nullTonum}: ${nullTonum}`);
+
+
+
+// //  BiGint to Boolean
+
+//  const bigint1 = Ziyad;
+//  const bigint2 = Boolean(bigint1)
+// console.log(`${typeof bigint1} : ${bigint1}`);
+//  console.log(`${typeof bigint2} : ${bigint2}`);
+
+// //  empty to Boolea
+
+//  const em = "";
+//  const em2 = Boolean(em)
+//  console.log(`${typeof em} : ${em}`);
+//  console.log(`${typeof em2} : ${em2}`);
+
+//  // // null to Boolean
+
+//  const nulllll = null;
+//  const nul = Boolean(null)
+//  console.log(`${typeof null} : ${null}`);
+//  console.log(`${typeof nul} : ${nul}`);
+
+
+
+// let str5 = "java script language";
+//  console.log(str);
+//  console.log(str.length);
+//  console.log(str.charAt(3));
+//  console.log(str.slice(5,6));
+// console.log(str.split(" ",2));
