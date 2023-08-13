faf363a3e5b171d9d16f6c790b660d9750b680b9
ziyad <ziadzaher2525@gmail.com>
2023-08-13 11:28:37 -0400

    hello

---
+++ day8/move.js
@@ -0,0 +1,11 @@
+let names = ["ziyad ","ail ", "samah","saud","braa"]
+
+console.log(names(0));
+names.unshift("ziyadd2")
+console.log(names);
+names.push("hello")
+console.log(names);
+names.shift()
+console.log(names);
+names.pop()
+console.log(names);
