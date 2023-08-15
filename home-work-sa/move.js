e64fb093c16a50219e03fb5816ebe225002672e8
ziyad <ziadzaher2525@gmail.com>
2023-08-15 13:34:33 -0400

    hello

---
+++ home-work-sa/move.js
@@ -0,0 +1,19 @@
+//  hola
+// first تمرين ({})
+ 
+ let name = prompt("enter your name :")
+ let age = prompt("your age")
+
+ if (age > 18) {
+     alert("wlcome !  " + name)
+ } else {
+     alert(" sorry you less than 18 ! " + name)
+ }
+// sc تمرين 
+ let name1 = prompt("your name")
+
+ if (name1 == "ziyad") {
+     alert("الاسمان متطابقان")
+ } else {
+     alert("الاسمان غير متطابقان") }
+     
