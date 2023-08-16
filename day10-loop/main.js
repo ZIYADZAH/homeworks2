7a0e33841d9ee703a782713ab69a9e645d51c01e
ziyad <ziadzaher2525@gmail.com>
2023-08-16 12:07:08 -0400

    hola

---
+++ day10-loop/main.js
@@ -0,0 +1,22 @@
+    // first تمرين
+
+
+// for(let i_2 = 0 ; i_2  <=  10  ; i_2++){
+//     console.log(i_2)
+// }
+// let i = 3;
+
+// // التمرين الثاني
+//     while (i >= 1) {
+//     console.log(i);
+//     i--;
+//     }
+//     let i_ = prompt("اكتب صفر")
+//     // =============== hgjlvdk hehg
+//     do {
+//     alert(i_);
+//     i_++;
+//     } while (i_ <= 5);
+    
+
+    
