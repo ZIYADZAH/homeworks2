1f089cd4da1f78e7d0e2a8accb652a467442f836
ziyad <ziadzaher2525@gmail.com>
2023-08-16 03:30:37 -0400

    hello

---
+++ codes-for-ali/javascriptpage/move.js
@@ -0,0 +1,73 @@
+// // console.log("hello");
+
+
+
+// // console.log(10==5);
+// // console.log(10==10);
+// // console.log(10==10n);
+// // console.log(10=="10");
+// // console.log("alide"=="bob");
+// // console.log(0==false);
+// // console.log(""==false);
+// // console.log(undefined==false);
+// // console.log(NaN==NaN);
+// // console.log(true==1);
+
+
+
+
+
+
+// // console.log(10===5);
+// // console.log(10===10);
+// // console.log(10===10n);
+// // console.log(10==="10");
+// // console.log("10"==="10");
+// // console.log("alide"==="bob");
+// // console.log(0===false);
+// // console.log(""===false);
+// // console.log(undefined===false);
+// // console.log(NaN===NaN);
+// // console.log(true===1);
+
+
+// // console.log(true && 1991);
+
+// // console.log(false&&1991);
+
+// // console.log(true || 1991);
+
+// // console.log(false ||1991);
+
+// // console.log(1&&5);
+
+// // console.log(""||"bob");
+// // console.log("alice"||"bob");
+
+
+
+
+
+ 
+// // console.log(2&&5);
+
+// // console.log(0&&5);
+
+// // console.log(1||5);
+
+// // alert(" and your age .....your old ,your name is ....")
+
+// // let names=prompt("what your name", )
+// // prompt
+
+// // let age=prompt(" hello  "+names+ " how old are you  ?") ;
+
+// // prompt("your" +age+ "....")
+
+
+// let name = prompt("Hi whats your name?")
+
+// let age = prompt("Hello Mr." + name + " tell me your age?")
+
+// // تحدي 
+// alert("and your age .. " + age + " years old " + " your name is " + name)
