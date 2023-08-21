2541520b5b6ff3b5dba2cd8b5b9b456df7d3fb4a
ziyad <ziadzaher2525@gmail.com>
2023-08-20 22:29:49 -0400

    home work for samah (founcion)(:

---
+++ fu--home work/main.js
@@ -0,0 +1,11 @@
+let javascriptBootcamp = (teacher1,teacher2)=>{
+    alert("This javaScript BootCamp with " +teacher1+" OK?")
+    alert("This javaScript BootCamp with "+ teacher2+" OK?");
+
+}
+javascriptBootcamp("ali","samah")
+let ave = (integer_,integer,double1,double2)=>{
+    alert(integer_+integer /2)
+    alert(double1+double2 /2)
+}
+ave(7,4,7,7)
